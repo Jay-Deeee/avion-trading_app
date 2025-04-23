18960d4b174c1477caaff53533ca94063fc478b6
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2025-04-23 21:07:32 +0800

    Connected api and added basic functions to pull values

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,7 @@
+class PortfoliosController < ApplicationController
+  def index
+    response = AvaApi.fetch_records(params[:symbol])
+    @symbol = response['Meta Data']['2. Symbol']
+    @stock_price = response.dig("Time Series (Daily)").values.first.dig("1. open")
+  end
+end
