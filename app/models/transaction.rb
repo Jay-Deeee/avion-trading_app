b74c57159a685d7110d74f346679c3e704fbe88a
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2025-04-24 19:01:35 +0800

    Added basic code to models and controllers

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :user
+
+  validates :symbol, presence: true
+  validates :shares, presence: true
+  validates :price, presence: true
+  validates :total, presence: true
+  validates :action_type, presence: true
 end
