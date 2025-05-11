aa15fa413a2b4e133f2a38ae2de4988f33dbef4b
Jay-Deeee <144668463+Jay-Deeee@users.noreply.github.com>
2025-05-11 22:45:28 +0800

    Added approved column for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_05_115455) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_11_143818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_05_115455) do
     t.boolean "is_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.check_constraint "balance >= 0::numeric", name: "balance_non_negative"
